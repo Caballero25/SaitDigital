3bda7c5883406e6863cce4708a771a05691e7e39
David Caballero <davidcame124@gmail.com>
2024-03-16 12:23:01 -0500

    facebook href

--- js/nav.js
+++ js/nav.js
@@ -19,4 +19,4 @@ document.getElementsByClassName("whatsaap")[0].href=whatsAppUrl
 var gmailUrl = "mailto:saittdocuments@gmail.com?subject=Cotización%20personalizada-%20SaitDigital&body=Estoy%20interesado%20en%20recibir%20una%20cotización%20personalizada%20para%20encargar%20mi%20proyecto%20web."
 document.getElementsByClassName("gmail")[0].href=gmailUrl
 
-document.getElementsByClassName("facebook")[0].href = "https://www.facebook.com/profile.php?id=61557040725532"
+document.getElementsByClassName("facebook")[0].href = "https://www.facebook.com/saitdigital"
